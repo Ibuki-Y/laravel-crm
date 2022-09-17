13c8c8156d5e660fe5a4a25c67d6edafdee53e8a
Ibuki-Y <ibu.love.drum@gmail.com>
2022-09-18 01:27:00 +0900

    Test Inertia

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
+
+<template>
+  <h1>InertiaTest</h1>
+  <a href="/">aタグ</a>
+  <br />
+  <Link href="/">Linkタグ</Link>
+  <br />
+  <Link :href="route('inertia.index')">v-bind:Linkタグ</Link>
+  <br />
+</template>
