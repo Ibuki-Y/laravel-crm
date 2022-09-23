7081f6a7cc3c37211c16e894178a221181dc52bc
Ibuki-Y <ibu.love.drum@gmail.com>
2022-09-24 00:03:30 +0900

    Add Customer Create

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,6 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'name',
+        'kana',
+        'tel',
+        'email',
+        'postcode',
+        'address',
+        'birthday',
+        'gender',
+        'memo',
+    ];
+
     public function scopeSearchCustomers($query, $input = null) {
         if (!empty($input)) {
             if (Customer::where('kana', 'like', $input . '%')
